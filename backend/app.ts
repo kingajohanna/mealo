477698631689cf816862a63bfbbf2994a6efe63d
Kinga Johanna Szabó <kinga.johanna.szabo@zooshgroup.com>
2023-03-20 16:19:21 +0100

    code cleaning

--- backend/app.ts
+++ backend/app.ts
@@ -27,13 +27,6 @@ https
     console.log(`Running on https://${HOST}:${PORT}`);
   });
 
-app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-
-  next();
-});
-
 var indexRouter = require("./routes/index");
 var recipeRouter = require("./routes/recipe");
 
